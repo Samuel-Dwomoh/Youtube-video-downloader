67f0d00b1d495fe45e5a4247bb71719731d8ca35
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-03-23 16:57:51 +0000

    Frontend setup done!

--- Frontend/youtube-video-downloader/src/App.jsx
+++ Frontend/youtube-video-downloader/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline bg-yellow-500">Setup completed!</h1>
+      <h1 className="text-3xl font-bold underline bg-yellow-500">Setup completed! </h1>
     </div>
   );
 }
