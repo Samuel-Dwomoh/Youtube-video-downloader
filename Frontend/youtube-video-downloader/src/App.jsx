1a0e3eacbee8f53ff7871840b770116ea96e4ac9
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-03-23 16:50:29 +0000

    Frontend setup complete!

---
+++ Frontend/youtube-video-downloader/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-yellow-500">Setup completed!</h1>
+    </div>
+  );
+}
+
+export default App;
