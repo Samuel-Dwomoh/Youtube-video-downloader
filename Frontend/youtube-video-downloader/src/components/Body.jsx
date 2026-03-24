023e0753cee19729768e358730dbf4bae264d790
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-03-24 16:56:39 +0000

    Updated preview modal

--- Frontend/youtube-video-downloader/src/components/Body.jsx
+++ Frontend/youtube-video-downloader/src/components/Body.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import load from "/public/load.svg";
+import download from "/public/download.svg";
 import { useState } from "react";
 
 function Body() {
   const [videoUrl, setVideoUrl] = useState("");
   const [preview, setPreview] = useState(null);
+  const [loading, setLoading] = useState(false)
 
   function handleInputChange(e){
     setVideoUrl(e.target.value);
@@ -13,6 +15,7 @@ function Body() {
 
   async function handleSubmit(e){
     e.preventDefault();
+    setLoading(true);
 
     try {
       const response = await fetch("http://127.0.0.1:8000/preview", {
@@ -25,6 +28,8 @@ function Body() {
       setPreview(data); 
     } catch (error) {
       console.error("Error fetching preview:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -67,25 +72,31 @@ function Body() {
 
             <form action="" method="POST" className="flex items-center w-full max-w-lg">
 
-                <input type="text" placeholder="Paste YouTube video URL here..." className="border-2 border-red-300 rounded-md p-2 w-full h-12 focus:outline-red-400 px-3 py-5" value={videoUrl} onChange={handleInputChange}/>
+                <input type="text" placeholder="Paste YouTube video URL here..." className="border-2 border-red-300 rounded-lg p-2 w-full h-13 focus:outline-red-400 px-3 py-5" value={videoUrl} onChange={handleInputChange}/>
 
                 <button type="submit" onClick={handleSubmit} className="bg-red-500 text-white rounded-md gap-x-2 pr-8 ml-2 py-1 hover:bg-red-600 h-12 flex items-center hover:scale-103"><img src={load} alt="" className="pl-2" />Preview</button>
             </form>
 
         </div>
 
-        {preview && (
-        <div className="flex flex-col items-center mt-6 gap-y-4 mb-45">
-          <p className="mt-2 font-semibold text-center pb-1 text-lg">{preview.title}</p>
+        {loading && (
+          <div className="mt-6">
+          <div className="loader"></div>
+          </div>
+        )}
+
+        {preview && !loading && (
+        <div className="flex flex-col items-center mt-14 gap-y-4 mb-45">
+          <p className="mt-2 font-bold text-center pb-1 text-xl text-red-700 ">{preview.title}</p>
           <div>
             <img
               src={preview.thumbnail}
               alt="Video thumbnail"
-              className="w-75 h-auto"
+              className="w-lg h-auto"
             />
           </div>
         
-            <button onClick={handleDownload} className="bg-red-500 text-white rounded-md px-4 ml-2 py-1 hover:bg-red-600 h-12">Download Video</button>
+            <button onClick={handleDownload} className="flex justify-center items-center gap-x-3 bg-red-500 text-white rounded-md px-4 ml-2 py-1 w-lg mb-5 hover:bg-red-600 h-12"> <img src={download} alt="" />Download Video</button>
         </div>
       )}
 
